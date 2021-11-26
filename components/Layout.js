5e1af10e3f2060ae03718f72dc4d8cc45b2cda1f
riefer02 <andrew.riefenstahl@gmail.com>
2021-11-26 13:22:43 -0600

    add Head to Layout for dynamic content'

--- components/Layout.js
+++ components/Layout.js
@@ -2,10 +2,15 @@ import React from 'react';
 import styles from '../assets/styles/components/_layout.module.scss';
 import PageHeader from '../components/PageHeader';
 import Navigation from '../components/Navigation';
+import Head from 'next/head';
 
 export default function Layout({ children, title }) {
   return (
     <div className={styles.layout}>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/tld-logo.svg" />
+      </Head>
       <Navigation />
       <PageHeader title={title} />
       {children}
