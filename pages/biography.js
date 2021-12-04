f118716c592543332c703e3a9d24929fc6bb0734
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-04 09:37:50 -0600

    new bandmates post type page biography

--- pages/biography.js
+++ pages/biography.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useInView } from 'react-intersection-observer';
+import Image from 'next/image';
 import Layout from '../components/Layout';
+import { getBandmates } from '../lib/fetchBandmates';
+import styles from '../assets/styles/pages/_biography.module.scss';
 
-export default function Biography() {
+export default function Biography({ bandmates }) {
+  console.log(bandmates);
   const { ref, inView, entry } = useInView({
     /* Optional options */
     threshold: 0,
@@ -10,9 +14,45 @@ export default function Biography() {
 
   return (
     <Layout title="Biography">
-      <div ref={ref}>
-        <h2>{`Header inside viewport ${inView}.`}</h2>
+      <div className="container">
+        <div ref={ref}>
+          <h2>{`Header inside viewport ${inView}.`}</h2>
+        </div>
+        <div className={styles.bioList__image}>
+          {bandmates.map((i) => {
+            const bandmate = i.node.postTypeBandmate;
+            const id = i.node.id;
+            return (
+              <div key={id} className={styles.bioItem}>
+                <div className={styles.bioItem__image}>
+                  <Image
+                    src={bandmate.image.sourceUrl}
+                    alt={`band member ${bandmate.name}`}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+                <div className={styles.bioItem__description}>
+                  <h4>{bandmate.instruments}</h4>
+                  <h3>{bandmate.name}</h3>
+                  <h5>{bandmate.quote}</h5>
+                  <p>{bandmate.biography}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(ctx) {
+  let bandmates = await getBandmates();
+
+  return {
+    props: {
+      bandmates,
+    },
+  };
 }
