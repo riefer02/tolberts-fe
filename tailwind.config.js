890625e1035885e6e5d4de888123469a1cd957af
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-18 18:01:18 -0600

    move devDep to Dep postcss + tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
-  content: ['./src/**/*.{html,js}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
     container: {
