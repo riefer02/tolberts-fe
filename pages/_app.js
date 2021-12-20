16fa1498f5f36b04f3d46a546a07435b4fb34a73
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-19 19:45:54 -0600

    add error handling in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Error from 'next/error';
 import 'tailwindcss/tailwind.css';
 import '../assets/styles/index.scss';
 
 function MyApp({ Component, pageProps }) {
+  if (pageProps.error) {
+    return (
+      <Error
+        statusCode={pageProps.error.statusCode}
+        title={pageProps.error.message}
+      />
+    );
+  }
   return <Component {...pageProps} />;
 }
 
