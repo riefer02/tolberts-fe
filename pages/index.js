6b4b65c76f02876c1c1d51c13daeff9090349953
riefer02 <andrew.riefenstahl@gmail.com>
2022-04-16 23:07:02 -0500

    reinstate events app

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
-// import EventApp from '../components/EventsApp';
-// import { formatQueriedEventData, sortByDate } from '../lib/events';
-// import { getEvents } from '../lib/fetchEvents';
+import EventApp from '../components/EventsApp';
+import { formatQueriedEventData, sortByDate } from '../lib/events';
+import { getEvents } from '../lib/fetchEvents';
 
 export default function Home({ events }) {
   return (
@@ -21,27 +21,27 @@ export default function Home({ events }) {
         />
         <meta property="og:image" content="/images/seo-image.jpeg" />
       </Head>
-      {/* {events?.length > 0 && <EventApp events={events} />} */}
-      <div className="grid place-items-center h-screen text-white text-lg max-w-md mx-auto p-8">
+      {events?.length > 0 && <EventApp events={events} />}
+      {/* <div className="grid place-items-center h-screen text-white text-lg max-w-md mx-auto p-8">
         We are experiencing technical difficulties with Amazon Web Services.
         Please be patient while Jeff Bezos works closely with our staff to solve
         this issue.
-      </div>
+      </div> */}
     </div>
   );
 }
 
-// export async function getServerSideProps(ctx) {
-//   const formattedEvents = [];
-//   const events = await getEvents();
+export async function getServerSideProps(ctx) {
+  const formattedEvents = [];
+  const events = await getEvents();
 
-//   for (const event of events) {
-//     formattedEvents.push(await formatQueriedEventData(event));
-//   }
+  for (const event of events) {
+    formattedEvents.push(await formatQueriedEventData(event));
+  }
 
-//   return {
-//     props: {
-//       events: sortByDate(formattedEvents),
-//     },
-//   };
-// }
+  return {
+    props: {
+      events: sortByDate(formattedEvents),
+    },
+  };
+}
