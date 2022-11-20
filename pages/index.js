ebb108770439111aecdc457dcbadca5700a51b6b
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-11-20 17:23:19 -0600

    update tolberts music robots.txt + seo open graph image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head';
+import Head from "next/head";
 // import EventApp from '../components/EventsApp';
-import { formatQueriedEventData, sortByDate } from '../lib/events';
-import { getEvents } from '../lib/fetchEvents';
-import dynamic from 'next/dynamic';
+import { formatQueriedEventData, sortByDate } from "../lib/events";
+import { getEvents } from "../lib/fetchEvents";
+import dynamic from "next/dynamic";
 
-const EventApp = dynamic(import('../components/EventsApp'));
+const EventApp = dynamic(import("../components/EventsApp"));
 
 export default function Home({ events }) {
   return (
@@ -22,7 +22,32 @@ export default function Home({ events }) {
           property="og:description"
           content="Live Music Events Calendar for Tolbert's Restaurant and Chili Parlor in Grapevine, Texas"
         />
-        <meta property="og:image" content="/images/seo-image.jpeg" />
+        <meta property="og:image" content="/images/seo-image.jpg" />
+
+        <meta property="og:url" content="https://tolbertsmusic.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Tolbert's Live Music Calendar" />
+        <meta
+          property="og:description"
+          content="Live Music Events Calendar for Tolbert's Restaurant and Chili Parlor in Grapevine, Texas"
+        />
+        <meta
+          property="og:image"
+          content="https://tolbertsmusic.com/images/seo-image.jpeg"
+        />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="tolbertsmusic.com" />
+        <meta property="twitter:url" content="https://tolbertsmusic.com" />
+        <meta name="twitter:title" content="Tolbert's Live Music Calendar" />
+        <meta
+          name="twitter:description"
+          content="Live Music Events Calendar for Tolbert's Restaurant and Chili Parlor in Grapevine, Texas"
+        />
+        <meta
+          name="twitter:image"
+          content="https://tolbertsmusic.com/images/seo-image.jpeg"
+        />
       </Head>
       {events?.length > 0 && <EventApp events={events} />}
       {/* <div className="grid place-items-center h-screen text-white text-lg max-w-md mx-auto p-8">
